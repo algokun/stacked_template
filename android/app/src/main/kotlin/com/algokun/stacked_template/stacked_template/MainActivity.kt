93af874267946033769e10b30dea94e2e477aa68
= <=>
2020-06-22 23:12:54 +0530

    🎉 feat: added flutter bear bones

---
+++ android/app/src/main/kotlin/com/algokun/stacked_template/stacked_template/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.algokun.stacked_template.stacked_template
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MainApp());
+
+class MainApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Stacked Template",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.deepOrange),
+      home: Container(),
+    );
+  }
+}
